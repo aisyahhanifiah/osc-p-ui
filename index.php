c5bebb1202664992c75fe64bcc267610be1916fb
aisyahhanifiah <aisyahhanifiah@live.com>
2020-10-09 09:39:00 +0800

    before friday meeting

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="kpkt.png">
+
+    <title>OSC 3 Plus Online</title>
+
+    <!-- Styles -->
+   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-4 my-3">
+			<H2>User: SP</H2>
+			<ul class="list-group">
+			  <li target="_blank" class="list-group-item"><a href="dashboard-sp.php">Dashboard</a></li>
+			  <li target="_blank" class="list-group-item"><a href="senaraiprojek-sp.php">Senarai Projek</a></li>
+			  <li target="_blank" class="list-group-item"><a href="maklumatprojek-sp.php">Maklumat Projek</a></li>
+			  <li target="_blank" class="list-group-item"><a href="lampirankerjabangunan.php">Lampiran Kerja Bangunan</a></li>
+			  <li target="_blank" class="list-group-item"><a href="lampirankerjatanah.php">Lampiran Kerja Tanah</a></li>
+			  <li target="_blank" class="list-group-item"><a href="semak.php">Semak dan Hantar Borang B</a></li>
+			  <li target="_blank" class="list-group-item"><a href="maklumatprojek-sp-p4.php">Maklumat Projek Proses 4</a></li>
+			  <li target="_blank" class="list-group-item"><a href="notisbertulis.php">Notis Bertulis</a></li>
+			  <li target="_blank" class="list-group-item"><a href="maklumatfi.php">Maklumat Kompaun</a></li>
+			  <li target="_blank" class="list-group-item"><a href="maklumatprojek-sp-p6.php">Maklumat Projek Proses 6</a></li>
+			  <li target="_blank" class="list-group-item"><a href="lampiranccc.php">Lampiran CCC</a></li>
+			  <li target="_blank" class="list-group-item"><a href="semak-p6.php">Semak dan Hantar Lampiran CCC</a></li>
+			</ul>
+		</div>
+		<div class="col-4 my-3">
+			<H2>User: OSC</H2>
+			<ul class="list-group">
+			  <li target="_blank" class="list-group-item"><a href="dashboard-osc.php">Dashboard</a></li>
+			  <li target="_blank" class="list-group-item"><a href="senaraiprojek-osc.php">Senarai Projek</a></li>
+			  <li target="_blank" class="list-group-item"><a href="maklumatprojek-osc.php">Maklumat Projek</a></li>
+			  <li target="_blank" class="list-group-item"><a href="aktivitiprojek-osc.php">Aktiviti</a></li>
+			  <li target="_blank" class="list-group-item"><a href="notifikasi-osc.php">Notifikasi Mula Kerja</a></li>
+			</ul>
+		</div>
+		<div class="col-4 my-3">
+			<H2>User: ATD</H2>
+			<ul class="list-group">
+			  <li target="_blank" class="list-group-item"><a href="dashboard-atd.php">Dashboard</a></li>
+			  <li target="_blank" class="list-group-item"><a href="senaraiprojek-atd.php">Senarai Projek</a></li>
+			  <li target="_blank" class="list-group-item"><a href="maklumatprojek-atd.php">Maklumat Projek</a></li>
+			  <li target="_blank" class="list-group-item"><a href="aktivitiprojek-atd.php">Aktiviti</a></li>
+			  <li target="_blank" class="list-group-item"><a href="maklumatkompaun.php">Maklumat Kompaun</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</html>
